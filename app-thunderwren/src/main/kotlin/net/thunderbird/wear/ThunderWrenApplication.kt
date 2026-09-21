b1e503bb161859de1128da40f1efce325c93c783
JUhalt <125321701+JUhalt@users.noreply.github.com>
2026-09-20 20:32:19 -0400

    fix: resolve double launcher icon, app logo, and Koin feature flag DI initialization

--- app-thunderwren/src/main/kotlin/net/thunderbird/wear/ThunderWrenApplication.kt
+++ app-thunderwren/src/main/kotlin/net/thunderbird/wear/ThunderWrenApplication.kt
@@ -1,12 +1,15 @@
 package net.thunderbird.wear
 
-import net.thunderbird.app.common.BaseApplication
+import net.thunderbird.app.common.FeatureFlagApplication
 import net.thunderbird.app.common.appCommonModule
 import net.thunderbird.wear.di.thunderWrenModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-class ThunderWrenApplication : BaseApplication() {
+class ThunderWrenApplication : FeatureFlagApplication() {
+    override val appName: String = "thunderwren"
+    override val appVersion: String = "0.1.0"
+
     override fun provideAppModule(): Module {
         return module {
             includes(appCommonModule)
