f21ed5dd074a6ee4fbc3ce56f9dc9517e4202b0b
JUhalt <125321701+JUhalt@users.noreply.github.com>
2026-09-20 20:44:26 -0400

    fix: resolve Koin FlagRegistryOverrideSerializer definition for Wear OS DI and set white icon background

--- app-thunderwren/src/main/kotlin/net/thunderbird/wear/di/ThunderWrenModule.kt
+++ app-thunderwren/src/main/kotlin/net/thunderbird/wear/di/ThunderWrenModule.kt
@@ -1,9 +1,21 @@
 package net.thunderbird.wear.di
 
+import net.thunderbird.core.featureflag.inject.featureFlagModule
+import net.thunderbird.core.featureflag.model.EmptyAppVariantOverride
+import net.thunderbird.core.featureflag.serialization.FlagRegistryOverrideSerializer
 import net.thunderbird.wear.ui.inbox.InboxViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val thunderWrenModule = module {
+    includes(featureFlagModule)
+
+    factory {
+        FlagRegistryOverrideSerializer(
+            k9Factory = EmptyAppVariantOverride,
+            thunderbirdFactory = EmptyAppVariantOverride,
+        )
+    }
+
     viewModel { InboxViewModel(preferences = get()) }
 }
